1c560a68f10a5014fc6018f76ca6803f8f25cbd1
walter lacoste <wlacoste@outlook.com>
2022-12-31 16:36:20 -0300

    modulo4

---
+++ codigo.php
@@ -0,0 +1,26 @@
+<?php
+$to = "info@hotmail.com";
+$subject = "Mail desde el formulario";
+$headers = "MIME-Version: 1.0" . "\r\n";
+$headers .= "Content-type:text/html;charset=UTF-8" .
+"\r\n";
+$nombre = $_POST['nombre'];
+$email = $_POST['email'];
+$to = $email;
+$comentarios = $_POST['comentarios'];
+$message = "
+<html>
+<head>
+<title>HTML</title>
+</head>
+<body>
+<h1>Información del formulario</h1>
+<p>Nombre: $nombre </p>
+<p>Email: $email </p>
+<p>Comentarios: $comentarios </p>
+</body>
+</html>";
+echo 'Gracias por comunicarse con nosotros';
+echo $message;
+mail($to, $subject, $message, $headers);
+?>
